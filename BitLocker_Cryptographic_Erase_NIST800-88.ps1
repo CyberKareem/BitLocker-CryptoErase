87c0e265e26e2b54bfdfd923f4ef2da9fd8db2e8
Abdullah Kareem <67718280+CyberKareem@users.noreply.github.com>
2025-05-03 11:19:32 +0300

    Update BitLocker_Cryptographic_Erase_NIST800-88.ps1

--- BitLocker_Cryptographic_Erase_NIST800-88.ps1
+++ BitLocker_Cryptographic_Erase_NIST800-88.ps1
@@ -10,7 +10,7 @@ Performs a cryptographic erase using BitLocker, compliant with NIST 800-88 rev.1
 .NOTES
 - THIS WILL MAKE DATA IRRECOVERABLE
 - Requires TPM 1.2+ and BitLocker-enabled drives
-- Must perform COLD shutdown after execution.
+- Must perform COLD shutdown after execution
 #>
 
 # Configuration

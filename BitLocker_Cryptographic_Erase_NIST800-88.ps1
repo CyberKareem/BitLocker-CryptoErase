6e36eb474ac7436cd06aea20bd49f0d521ca2bb8
Abdullah Kareem <67718280+CyberKareem@users.noreply.github.com>
2025-05-28 22:13:31 +0300

    Update BitLocker_Cryptographic_Erase_NIST800-88.ps1

--- BitLocker_Cryptographic_Erase_NIST800-88.ps1
+++ BitLocker_Cryptographic_Erase_NIST800-88.ps1
@@ -10,7 +10,7 @@
 .DESCRIPTION
     This script performs a cryptographic erase on all internal drives using BitLocker,
     ensuring data is permanently deleted and unrecoverable in accordance with
-    NIST Special Publication 800-88 Revision 1 guidelines.
+    NIST Special Publication 800-88 Revision 1 guidelines
     It ncludes handling of unallocated spaces to ensure complete drive erasure
 
 .NOTES
